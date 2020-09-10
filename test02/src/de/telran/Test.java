73775783976839faa48323187129a4ed42bcf521
dina-03 <dingrafikweb@gmail.com>
2020-09-10 10:36:43 +0200

    test02

--- test02/src/de/telran/Test.java
+++ test02/src/de/telran/Test.java
@@ -3,5 +3,6 @@ package de.telran;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Это тест создания нового проекта в новой ветке");
+        System.out.println("wieder test");
     }
 }
